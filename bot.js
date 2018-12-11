46ceeb042e889e97f3030f9b0f6fad92900585a3
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-11 01:31:15 +0000

    updated

---
+++ bot.js
@@ -0,0 +1,44 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs");
+const http = require("http");
+const express = require("express");
+const app = express();
+
+const discord_token = process.env.TOKEN;
+const prefix = process.env.PREFIX;
+
+client.on("ready", () => {
+  console.log("Connected!");
+});
+
+app.get("/", (request, response) => {
+  console.log("Ping received!");
+  response.sendStatus(200);
+});
+
+// This keeps the bot running 24/7
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`https://phantomdevelopment-discord-bot.herokuapp.com/`);
+}, 280000);
+
+
+client.on("message", message => {
+  if (message.author.bot) return;
+  if (message.content.indexOf(prefix) !== 0) return;
+
+  // This is the best way to define args. Trust me.
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  // The list of if/else is replaced with those simple 2 lines:
+  try {
+    let commandFile = require(`./commands/${command}.js`);
+    commandFile.run(client, message, args);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+client.login(discord_token);
