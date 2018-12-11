d259714eda441d022d149684f545f9c8f9337ca6
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-11 02:25:34 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const app = express();
 const discord_token = process.env.TOKEN;
 const prefix = process.env.PREFIX;
 const pass = process.env.PASSWORD;
-const version = "0.0.02a";
+const version = "0.0.03a";
 const logs = "521873059204825172";
 
 app.get("/", (request, response) => {
@@ -25,6 +25,13 @@ setInterval(() => {
 client.on("ready", () => {
 	client.user.setPresence({ game: { name: `${prefix}help | V${version}`, type: 0} });
 	client.channels.get(`${logs}`).send(`Starting up....`);
+	const embed = new Discord.RichEmbed()
+	.setAuthor("Developer: aj2958#7948", "https://t0.rbxcdn.com/e25a771f37859b7c227944230596bae6")
+	.setColor(0x00AE86)
+	.addField("Created on:", `11st December 2018`)
+	.setFooter(`Version: ${version}`, "https://t0.rbxcdn.com/e25a771f37859b7c227944230596bae6");
+	client.channels.get(`${logs}`).send({embed});
+	client.channels.get(`${logs}`).send(`Bot Successfully Started.`);
 });
 
 client.on("message", message => {
