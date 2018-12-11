70f35ae62c66bee3d287fa8e43c09e7a01c1b907
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-11 04:07:56 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,4 +44,18 @@ client.on("message", message => {
   }
 });
 
+var Connection = require('tedious').Connection;  
+var config = {  
+    userName: process.env.SQLUSER,  
+    password: process.env.SQLPASS,  
+    server: process.env.SQLHOST,
+	database: "d2f96qlf22nhsn";
+	port: `5432`,
+    options: {encrypt: true}  
+};  
+var connection = new Connection(config);  
+connection.on('connect', function(err) {  
+	client.channels.get(`${logs}`).send(`SQL Connection Success.`);
+});  
+
 client.login(discord_token);
