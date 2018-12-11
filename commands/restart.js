213c487cbcbce5c20dcbadecf0af5d542e6c6330
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-11 04:44:59 +0000

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -1,7 +1,11 @@
 const Discord = module.require("discord.js");
+const client = new Discord.Client();
+const logs = "521873059204825172";
 
 module.exports.run = async (client, message, args) => {
 	if (message.author.id !== '501649887411175435') return;
+	message.channel.send(`Bot Restarting.`)
+	client.channels.get(`${logs}`).send(`Bot Restarting....`);
     process.exit(1);
 }
 
