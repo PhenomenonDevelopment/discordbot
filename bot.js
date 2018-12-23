ce33ced425ccc53d0dd4aaa418107b68590941ad
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-23 16:20:48 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,32 +10,6 @@ const prefix = process.env.PREFIX;
 const logs = "521873059204825172";
 const version = process.env.VERSION;
 
-const Sharder = require('eris-sharder').Master;
-const sharder = new Sharder(`${discord_token}`, `/bot.js`, {
-  stats: false,
-  debug: false,
-  guildsPerShard: "1500",
-  name: "Phenomenon",
-  webhooks: {
-    shard: {
-      id: "526426368834011156",
-      token: "Ia3vDF5Mm_ijVhW2Cu4uTPfiaTIaEaegtpDXMANgrk9jx4enipArQvpH0Z8kLgpo30Ky"
-    },
-     cluster: {
-      id: "526426368834011156",
-      token: "Ia3vDF5Mm_ijVhW2Cu4uTPfiaTIaEaegtpDXMANgrk9jx4enipArQvpH0Z8kLgpo30Ky"
-    }
-  },
-  clientOptions: {
-      messageLimit: 150,
-      defaultImageFormat: "png"
-  }
-});
-
-sharder.on("stats", stats => {
-  console.log(stats);
-});
-
 app.listen(process.env.PORT);
 setInterval(() => {
   http.get(`http://phantomdevelopment-discord-bot.herokuapp.com/`);
