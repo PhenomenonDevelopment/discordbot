2a233baa3b4a19262ad9dfda29db188c7f0b9b50
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-23 15:28:31 +0000

    Update whataremyperms.js

--- commands/whataremyperms.js
+++ commands/whataremyperms.js
@@ -1,15 +1,8 @@
 const Discord = module.require("discord.js");
 const client = new Discord.Client();
-const logs = "521873059204825172";
-const prefix = process.env.PREFIX;
-const version = process.env.VERSION;
 
 module.exports.run = async (client, message, args) => {
-	const listedChannels = []; 
-	message.member.permissions.forEach(channel => { 
-		listedChannels.push(channel.name);
-	});
-	message.channel.send(`You have access to: ${listedChannels.join(',')}`);
+	message.channel.send(`You have access to: ${message.member.permissions}`);
 }
 
 module.exports.help = {
