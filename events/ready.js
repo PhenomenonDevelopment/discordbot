581d6e4ef9439651f6493e05e549bcda1fb9b2f5
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-23 14:57:27 +0000

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,7 @@ const prefix = process.env.PREFIX;
 const version = process.env.VERSION;
 module.exports = (client) => {
 	client.channels.get(`${logs}`).send(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
-	client.channels.get(`${logs}`).send(client.channels.id.array().sort());
+	client.channels.array().sort()
+	.then(wb => message.channel.send(`${wb.id}`)).catch(console.error)
 	client.user.setPresence({ game: { name: `${prefix}help | V${version}`, type: 0} });
 }
