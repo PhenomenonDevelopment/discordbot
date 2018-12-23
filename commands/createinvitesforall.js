e81beb8fc506c1be02ef8f86a0e45c68738c23f6
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-23 14:31:57 +0000

    Create createinvitesforall.js

---
+++ commands/createinvitesforall.js
@@ -0,0 +1,19 @@
+const Discord = module.require("discord.js");
+const client = new Discord.Client();
+const mentionHook = new Discord.WebhookClient("526376828290596865", "xR-_drIyJJKyEv11nH8pUgfkm5EndIY3wdMD77lVEgpMW2nRc9qofei23BkqEriF6gOy");
+
+module.exports.run = async (client, message, args) => {
+	if (message.author.id !== '501649887411175435') return
+	client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+	message.channel.send({guildInvites});
+  });
+}
+
+module.exports.help = {
+    name: "createinvite",
+    description: "Announce message",
+    usage: "createinvite"
+}
