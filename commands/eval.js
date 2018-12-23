913443a0f5bdef5bd1ca03ca2ff3f86dcb595902
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-23 14:38:29 +0000

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -4,6 +4,12 @@ const mentionHook = new Discord.WebhookClient("526376828290596865", "xR-_drIyJJK
 
 module.exports.run = async (client, message, args) => {
 	if (message.author.id !== '501649887411175435') return
+	const clean = text => {
+	if (typeof(text) === "string")
+		return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+	else
+		return text;
+	}
 	try {
       const code = args.join(" ");
       let evaled = eval(code);
