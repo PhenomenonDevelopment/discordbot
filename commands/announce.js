4578964dcfb6475099603be58e624af2a924afad
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-23 12:37:46 +0000

    Create announce.js

---
+++ commands/announce.js
@@ -0,0 +1,14 @@
+const Discord = module.require("discord.js");
+const client = new Discord.Client();
+const mentionHook = new Discord.WebhookClient("526376828290596865", "xR-_drIyJJKyEv11nH8pUgfkm5EndIY3wdMD77lVEgpMW2nRc9qofei23BkqEriF6gOy");
+
+module.exports.run = async (client, message, args) => {
+	if (message.author.id !== '501649887411175435') return
+	mentionHook.send(args);
+}
+
+module.exports.help = {
+    name: "announce",
+    description: "Announce message",
+    usage: "announce [msg]"
+}
