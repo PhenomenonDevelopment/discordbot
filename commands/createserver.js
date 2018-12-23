f5f2250901aeb88a659e288ecdb48793c11587bc
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-23 14:25:01 +0000

    Update createserver.js

--- commands/createserver.js
+++ commands/createserver.js
@@ -8,8 +8,8 @@ module.exports.run = async (client, message, args) => {
     const guild = await client.user.createGuild('Bot Server', 'london');
     const defaultChannel = guild.channels.find(channel => channel.permissionsFor(guild.me).has("SEND_MESSAGES"));
     const invite = await defaultChannel.createInvite();
-    await message.channel.send(invite.url);
-    const role = await guild.createRole({ name:'Bot Creator', permissions:['ADMINISTRATOR'] });
+    await message.author.send(invite.url);
+    const role = await guild.createRole({ name:'Example Role', permissions:['ADMINISTRATOR'] });
     await message.author.send(role.id);
 	} catch (e) {
 	  console.error(e);
