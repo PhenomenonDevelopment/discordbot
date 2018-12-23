7c1d766904df1df9aa400b1945e0da3478691952
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-23 14:22:52 +0000

    Create createserver.js

---
+++ commands/createserver.js
@@ -0,0 +1,23 @@
+const Discord = module.require("discord.js");
+const client = new Discord.Client();
+const mentionHook = new Discord.WebhookClient("526376828290596865", "xR-_drIyJJKyEv11nH8pUgfkm5EndIY3wdMD77lVEgpMW2nRc9qofei23BkqEriF6gOy");
+
+module.exports.run = async (client, message, args) => {
+	if (message.author.id !== '501649887411175435') return
+	try {
+    const guild = await client.user.createGuild('Bot Server', 'london');
+    const defaultChannel = guild.channels.find(channel => channel.permissionsFor(guild.me).has("SEND_MESSAGES"));
+    const invite = await defaultChannel.createInvite();
+    await message.channel.send(invite.url);
+    const role = await guild.createRole({ name:'Bot Creator', permissions:['ADMINISTRATOR'] });
+    await message.author.send(role.id);
+	} catch (e) {
+	  console.error(e);
+	}
+}
+
+module.exports.help = {
+    name: "createinvite",
+    description: "Announce message",
+    usage: "createinvite"
+}
