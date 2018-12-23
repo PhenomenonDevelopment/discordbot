5b2c3968a6d9adf85ec96523208db93fe15a41f4
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-23 13:25:12 +0000

    Create createinvite.js

---
+++ commands/createinvite.js
@@ -0,0 +1,15 @@
+const Discord = module.require("discord.js");
+const client = new Discord.Client();
+const mentionHook = new Discord.WebhookClient("526376828290596865", "xR-_drIyJJKyEv11nH8pUgfkm5EndIY3wdMD77lVEgpMW2nRc9qofei23BkqEriF6gOy");
+
+module.exports.run = async (client, message, args) => {
+	if (message.author.id !== '501649887411175435') return
+	message.channel.createInvite()
+	.then(wb => message.author.send(`${wb.code}`)).catch(console.error))
+}
+
+module.exports.help = {
+    name: "createinvite",
+    description: "Announce message",
+    usage: "createinvite"
+}
