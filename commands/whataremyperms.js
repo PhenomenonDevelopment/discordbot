e55ae78273111a390b023b25d3b50866529cc109
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-23 15:36:55 +0000

    Update whataremyperms.js

--- commands/whataremyperms.js
+++ commands/whataremyperms.js
@@ -2,6 +2,17 @@ const Discord = module.require("discord.js");
 const client = new Discord.Client();
 
 module.exports.run = async (client, message, args) => {
+	let perms = message.member.permissions;
+	const embed = new Discord.RichEmbed()
+	.setAuthor("Developer: aj2958#7948", "https://t0.rbxcdn.com/e25a771f37859b7c227944230596bae6")
+	.setColor(0x00AE86)
+	.addField("ADMINISTRATOR:", perms.has("ADMINISTRATOR"))
+	.addField("MANAGE_CHANNELS:", perms.has("MANAGE_CHANNELS"))
+	.addField("MANAGE_CHANNELS:", perms.has("MANAGE_CHANNELS"))
+	.addField("MANAGE_GUILD:", perms.has("MANAGE_GUILD"))
+	.addField("MANAGE_CHANNELS:", perms.has("MANAGE_CHANNELS"))
+	.setFooter(`Version: ${version}`);
+	message.channel.send({embed});
 	message.channel.send(`You have access to: ${message.member.permissions}`);
 }
 
