fbfaeea41c28ba62c4962935d81a5b9bfd182f62
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-23 14:35:01 +0000

    addeval

--- commands/eval.js
+++ commands/eval.js
@@ -4,12 +4,17 @@ const mentionHook = new Discord.WebhookClient("526376828290596865", "xR-_drIyJJK
 
 module.exports.run = async (client, message, args) => {
 	if (message.author.id !== '501649887411175435') return
-	client.guilds.forEach(g => {
-    g.fetchInvites().then(guildInvites => {
-      invites[g.id] = guildInvites;
-    });
-	message.channel.send({guildInvites});
-  });
+	try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+ 
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+ 
+      message.channel.send(clean(evaled), {code:"xl"});
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
 }
 
 module.exports.help = {
