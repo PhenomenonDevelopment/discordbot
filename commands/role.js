fa2637808b00592ded7fc54b604a0612d5a51c5c
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-24 08:10:22 +0000

    update

--- commands/role.js
+++ commands/role.js
@@ -7,9 +7,9 @@ module.exports.run = async (client, message, args) => {
   if (message.author.id !== '501649887411175435') return message.reply("Sorry pal, you can't do that.");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if(!rMember) return message.reply("Couldn't find that user, yo.");
-  let role = args.join(" ").slice(22);
+  let role = args[2];
   if(!role) return message.reply("Specify a role!");
-  let gRole = message.guild.roles.find(`name`, role);
+  let gRole = message.guild.roles.find(`id`, role);
   if(!gRole) return message.reply("Couldn't find that role.");
 
   if(rMember.roles.has(gRole.id)) return message.reply("They already have that role.");
