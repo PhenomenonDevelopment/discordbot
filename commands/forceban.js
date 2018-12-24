fc658c2bfc4181c80f9a00148f93d702ac7355b5
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-24 10:36:40 +0000

    Update forceban.js

--- commands/forceban.js
+++ commands/forceban.js
@@ -9,20 +9,19 @@ module.exports.run = async (client, message, args) => {
 	let member = args[0];
 	if (isNaN(member)) return message.reply('Please give a user ID to ban');
 
-	const modlog = message.guild.channels.find('id', modlog);
-	const caseNum = await caseNumber(client, modlog);
+	const modlogs = message.guild.channels.find('id', modlog);
 	const reason = args.splice(1, args.length).join(' ') || `Awaiting moderator's input. Use ${guildSettings.prefix}reason ${caseNum} <reason>.`;
 
 	message.guild.ban(member).then(() => {
 		message.reply(`${member} has been force banned by ${message.author.tag} because: ${reason}`);
-		if (!modlog) return console.log('modLogChannel does not exist on this server');
+		if (!modlogs) return console.log('modLogChannel does not exist on this server');
 		const embed = new Discord.RichEmbed()
 			.setColor('RED')
 			.setTitle('User force banned')
 			.addField(`User`, `${member}`, true)
 			.addField(`Moderator`, `${message.author.tag} (${message.author.id})`, true)
 			.addField(`Reason`, `${reason}`, true)
-		modlog.send({ embed })
+		modlogs.send({ embed })
 			.then(() => {
 				client.log('log', `${message.guild.name}/#${message.channel.name} (${message.channel.id}): ${member} was force banned by ${message.author.tag} (${message.author.id})`, 'CMD');
 			})
