d0b36cd4bfb78dbd159b88f1882ba5b80923ded9
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-24 07:41:40 +0000

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,7 +1,8 @@
 // The MESSAGE event runs anytime a message is received
 // Note that due to the binding of client to every event, every event
 // goes `client, other, args` when this function is run.
-//const Discord = require('discord.js');
+const Discord = require('discord.js');
+const prefix = process.env.PREFIX;
 module.exports = (client, message) => { // eslint-disable-line complexity
   if (message.author.bot) return;
   if (message.content.indexOf(prefix) !== 0) return;
