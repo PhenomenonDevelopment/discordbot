c815a4ff0cc6ab39f877f600417472d7c07733cf
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-24 04:42:02 +0000

    update

--- bot.js
+++ bot.js
@@ -10,6 +10,9 @@ const prefix = process.env.PREFIX;
 const logs = "521873059204825172";
 const version = process.env.VERSION;
 
+const levels = require("./levels.json");
+typeof levels; // object
+
 app.listen(process.env.PORT);
 setInterval(() => {
   http.get(`http://phenomenon-discord-bot.herokuapp.com/`);
@@ -31,7 +34,15 @@ client.on("message", message => {
   if (message.content.indexOf(prefix) !== 0) return;
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-
+  
+  if(!levels[message.author.tag]){ 
+	levels[message.author.id] = { 
+		money: 0 
+	};
+  }
+  levels[message.author.tag].money += 25;
+  fs.writeFileSync("./levels.json", JSON.stringify(levels));
+  
   try {
     let commandFile = require(`./commands/${command}.js`);
     commandFile.run(client, message, args);
