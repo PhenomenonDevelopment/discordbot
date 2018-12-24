4700cbae9616235d58b22cb89d1dd93fedcf2545
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-24 08:37:57 +0000

    Update createserver.js

--- commands/createserver.js
+++ commands/createserver.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const mentionHook = new Discord.WebhookClient("526376828290596865", "xR-_drIyJJKyEv11nH8pUgfkm5EndIY3wdMD77lVEgpMW2nRc9qofei23BkqEriF6gOy");
 
 module.exports.run = async (client, message, args) => {
-	//if (message.author.id !== '501649887411175435') return
+	if (message.author.id !== '501649887411175435') return message.reply("Sorry to stop abuse only Phenomenon#7948 can create servers");
     const guild = await client.user.createGuild('Bot Server', 'london');
     const defaultChannel = guild.channels.find(channel => channel.name === "general");
     const invite = await defaultChannel.createInvite();
