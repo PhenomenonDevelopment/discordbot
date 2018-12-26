0cf9b632361ba64d31092a9b818089cef668a808
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-26 23:19:44 +0000

    xp fix?

--- bot.js
+++ bot.js
@@ -45,18 +45,8 @@ fs.readdir("./events/", (err, files) => {
 
 client.on("message", async message => {
   if (message.author.bot) return;
-  con.query(`SELECT * FROM xp WHERE id = '${message.author.id}'`, (err,rows) => {
-	if(err) return console.error(err);
-	let sql;
-	if(rows.length <1) {
-	  sql = `INSERT INTO xp (id,xp) VALUES('${message.author.id}', ${generateXp()})`
-	} else {
-	  let xp = rows[0].xp;
-	  sql = `UPDATE xp SET xp = ${xp + generateXp()} WHERE id = '${message.author.id}'`
-	}
-	con.query(sql, console.log);
-  });
-  if (message.content.indexOf(prefix) !== 0) return;
+  
+  if (message.content.indexOf(prefix) !== 0){
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
@@ -74,6 +64,18 @@ client.on("message", async message => {
     client.channels.get(modlogs).send({embed});
   } catch (err) {
     console.error(err);
+  }} else {
+	  con.query(`SELECT * FROM xp WHERE id = '${message.author.id}'`, (err,rows) => {
+	  if(err) return console.error(err);
+	  let sql;
+	  if(rows.length <1) {
+	    sql = `INSERT INTO xp (id,xp) VALUES('${message.author.id}', ${generateXp()})`
+	  } else {
+	    let xp = rows[0].xp;
+	    sql = `UPDATE xp SET xp = ${xp + generateXp()} WHERE id = '${message.author.id}'`
+	  }
+	  con.query(sql);
+  });
   }
 });
 
--- commands/xp.js
+++ commands/xp.js
@@ -1,12 +1,20 @@
 const Discord = module.require("discord.js");
 const client = new Discord.Client();
 
-module.exports.run = async (client, message, args, con) => {
+var con = mysql.createConnection({
+  host: "uf63wl4z2daq9dbb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+  user: "lzpf45xmkds90fhi",
+  password: process.env.DBP,
+  database: "krntwx8gh9aecxvw"
+});
+
+module.exports.run = async (client, message, args) => {
 	con.query(`SELECT * FROM xp WHERE id = '${message.author.id}'`, (err,rows) => {
 		if(err) throw err;
 		let xp = rows[0].xp;
 		message.channel.send(xp);
 	});
+	con.end();
 }
 
 module.exports.help = {
