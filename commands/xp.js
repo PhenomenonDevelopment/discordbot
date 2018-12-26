eb7385664c17040cf01a0d635f76906df9c67d2a
aj2958 <40498523+aj2958@users.noreply.github.com>
2018-12-26 23:36:05 +0000

    Update xp.js

--- commands/xp.js
+++ commands/xp.js
@@ -11,7 +11,7 @@ var con = mysql.createConnection({
 
 module.exports.run = async (client, message, args) => {
 	con.query(`SELECT * FROM xp WHERE id = '${message.author.id}'`, (err,rows) => {
-		if(err) return console.log;
+		if(err) throw err;
 		let xp = rows[0].xp;
 		message.channel.send(xp);
 	});
