ae7883fedc7e0aec077c60c912f3711a61cf08be
aj2958 <46085658+PhenomenonDevelopment@users.noreply.github.com>
2019-01-31 20:54:46 +0000

    update

--- bot.js
+++ bot.js
@@ -8,11 +8,35 @@ const app = express();//
 const discord_token = process.env.TOKEN;
 const prefix = process.env.PREFIX;
 const version = process.env.VERSION;
+const version = process.env.COOKIE;
 
 app.listen(process.env.PORT);
 setInterval(() => {
 http.get(`${process.env.HOST}`);
 }, 280000);
+
+const rbx = require("noblox.js")
+async function startApp () {
+    await rbx.cookieLogin("_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_F9F1EA531adk")
+    // Do everything else, calling functions and the like.
+    let currentUser = await rbx.getCurrentUser()
+}
+
+var blacklist = [1, 261]
+var evt = rbx.onJoinRequestHandle(18)
+evt.on('data', function (request) {
+  rbx.getIdFromUsername(request.username).then(function (id) {
+    for (var i = 0; i < blacklist.length; i++) {
+      if (blacklist[i] === id) {
+        evt.emit('handle', request, false);
+        return;
+      }
+    }
+//    evt.emit('handle', request, true, function () {
+//      rbx.message(id, 'Welcome', 'Welcome to my group');
+//    });
+  });
+});
 
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
--- commands/about.js
+++ commands/about.js
@@ -9,10 +9,10 @@ module.exports.run = async (client, message, args) => {
 	.setAuthor("Developer: aj2958#7948", "https://t0.rbxcdn.com/e25a771f37859b7c227944230596bae6")
 	.setColor(0x00AE86)
 	.addField("Scripting Language:", `JavaScript`)
-	.addField("Depedances:", `DiscordJS\nRobloxJS`)
+	.addField("Depedances:", `DiscordJS\nRobloxJS\nNobloxJS`)
 	.addField("Servers:", client.guilds.map(g=>g.id).join(', '))
 	.addField("Ping:", `Server ${Date.now() - message.createdTimestamp}ms | API ${Math.round(client.ping)}ms`)
-	.addField("Created on:", `11st December 2018`)
+	.addField("Created on:", `30 January 2019`)
 	.setFooter(`Version: ${version}`);
 	message.channel.send({embed});
 }
